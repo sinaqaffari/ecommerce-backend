5a9856adfab285ff4a2984feff550d1b0da28b79
sinaqaffari <qaffarisina@gmail.com>
2025-10-28 14:04:26 +0330

    Added CustomTokenObtainPairView in users.views

--- django_core/urls.py
+++ django_core/urls.py
@@ -17,17 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users.views import UserDetailView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView
+from users.views import CustomTokenObtainPairView
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/orders/', include('orders.urls')),
     path('api/payments/', include('payments.urls')),
     path('api/products/', include('products.urls')),
     path('api/cart/', include('cart.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name="token_obtain"), #JWT endpoints
+    path('api/token/', CustomTokenObtainPairView.as_view(), name="token_obtain"), #JWT endpoints
     path('api/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"), #JWT endpoints
     path('api/users/<int:pk>/', UserDetailView.as_view(), name="user_detail_view"),
 ]
--- users/views.py
+++ users/views.py
@@ -2,9 +2,19 @@ from rest_framework import generics
 from .models import CustomUser 
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.response import Response
+from rest_framework_simplejwt.settings import api_settings as SIMPLE_JWT
 
 
 class UserDetailView(generics.RetrieveAPIView):
     queryset = CustomUser.objects.all()
     serializer_class =  UserSerializer
     permission_classes = [IsAuthenticated]
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request ,*args, **kwargs)
+        response.data['expires_in'] = SIMPLE_JWT.ACCESS_TOKEN_LIFETIME.total_seconds()
+        return response
